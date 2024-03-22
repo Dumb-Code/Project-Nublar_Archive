e07860b1b7f1a481fbc42576bb45c6cb9d663cde
TheRealEmissions <unitedrealm@hotmail.com>
2024-03-22 19:51:14 +0000

    of() methods in DumbItemStack

--- src/main/java/net/dumbcode/projectnublar/core/items/DumbItemStack.java
+++ src/main/java/net/dumbcode/projectnublar/core/items/DumbItemStack.java
@@ -2,10 +2,26 @@ package net.dumbcode.projectnublar.core.items;
 
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ItemLike;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 public class DumbItemStack {
     private final ItemStack itemStack;
+
+    @Contract(value = "_ -> new", pure = true)
+    public static @NotNull DumbItemStack of(@NotNull ItemStack itemStack) {
+        return new DumbItemStack(itemStack);
+    }
+
+    @Contract("_ -> new")
+    public static @NotNull DumbItemStack of(@NotNull ItemLike item) {
+        return new DumbItemStack(item);
+    }
+
+    @Contract("_, _ -> new")
+    public static @NotNull DumbItemStack of(@NotNull ItemLike item, int count) {
+        return new DumbItemStack(item, count);
+    }
 
     public DumbItemStack(@NotNull ItemStack itemStack) {
         this.itemStack = itemStack;
