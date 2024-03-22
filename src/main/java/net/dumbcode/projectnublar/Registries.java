aea948927ec9b6859339b54f07c055d1ac3f8b4b
TheRealEmissions <unitedrealm@hotmail.com>
2024-03-22 17:00:06 +0000

    Fixed issue preventing mod from loading

--- src/main/java/net/dumbcode/projectnublar/Registries.java
+++ src/main/java/net/dumbcode/projectnublar/Registries.java
@@ -8,9 +8,13 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 import static net.dumbcode.projectnublar.ProjectNublar.MODID;
 
-public class Registries {
+public final class Registries {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
     // Create a Deferred Register to hold Items which will all be registered under the "projectnublar" namespace
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(net.minecraft.core.registries.Registries.CREATIVE_MODE_TAB, MODID);
+
+    private Registries() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
 }
