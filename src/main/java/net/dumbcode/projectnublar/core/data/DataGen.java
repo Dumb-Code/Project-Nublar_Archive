0e3c26c9fb333f393db06c6df70b5e0c1040b7e0
TheRealEmissions <unitedrealm@hotmail.com>
2024-03-23 03:51:36 +0000

    data gen

--- src/main/java/net/dumbcode/projectnublar/core/data/DataGen.java
+++ src/main/java/net/dumbcode/projectnublar/core/data/DataGen.java
@@ -6,7 +6,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.jetbrains.annotations.NotNull;
 
-@Mod.EventBusSubscriber(modid = ProjectNublar.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+@Mod.EventBusSubscriber(modid = ProjectNublar.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DataGen {
     @SubscribeEvent
     public void onDataGather(@NotNull GatherDataEvent event) {
