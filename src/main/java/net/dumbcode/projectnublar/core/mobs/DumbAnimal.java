16aa827bd23acdd3dff0d4418c33b060a3355399
therealemissions <me@liammartin.site>
2024-03-27 20:46:48 +0000

    DumbAnimal abstract class

---
+++ src/main/java/net/dumbcode/projectnublar/core/mobs/DumbAnimal.java
@@ -0,0 +1,14 @@
+package net.dumbcode.projectnublar.core.mobs;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.level.Level;
+import software.bernie.geckolib.animatable.GeoEntity;
+
+public abstract class DumbAnimal extends Animal implements GeoEntity, DumbEntity {
+    @SuppressWarnings("unchecked")
+    protected DumbAnimal(EntityType<? extends Entity> pEntityType, Level pLevel) {
+        super((EntityType<? extends Animal>) pEntityType, pLevel);
+    }
+}
