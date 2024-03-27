ee86563167be5388e82217bffe91a8d93e580b24
therealemissions <me@liammartin.site>
2024-03-27 02:23:21 +0000

    public -> protected

--- src/main/java/net/dumbcode/projectnublar/core/blocks/DumbBlock.java
+++ src/main/java/net/dumbcode/projectnublar/core/blocks/DumbBlock.java
@@ -3,7 +3,7 @@ package net.dumbcode.projectnublar.core.blocks;
 import net.minecraft.world.level.block.Block;
 
 public abstract class DumbBlock extends Block implements IDumbBlock {
-    public DumbBlock(Properties properties) {
+    protected DumbBlock(Properties properties) {
         super(properties);
     }
 }
